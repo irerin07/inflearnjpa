74ef97204e82f1429e035639eb5d3f5bd0c41582
irerin07 <counterchord89@gmail.com>
2020-12-22 22:29:10 +0900

    1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,23 +15,29 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-//            Member member = new Member();
-//
-//            member.setId(2L);
-//            member.setName("HelloB");
-            Member member = em.find(Member.class, 1L);
-            System.out.println("member.getId() = " + member.getId());
+            Member m1 = new Member(9L, "aa");
+            Member m2 = new Member(10L, "bb");
+
+            em.persist(m1);
+            em.persist(m2);
+
+            Member member = em.find(Member.class, 9L);
             System.out.println("member.getName() = " + member.getName());
 
-//            em.remove(member);
-            member.setName("HelloJPA");
+            System.out.println("===================");
 
-//            em.persist(member); //update쿼리는 persist할 필요가 없다.
+            em.remove(m1);
+            em.find(Member.class, 9L);
+            System.out.println("member.getName() = " + member.getName());
 
+            System.out.println("------------------");
+
+            
             tx.commit();
-        } catch (Exception e){
+            em.find(Member.class, 9L);
+        } catch (Exception e) {
             tx.rollback();
-        }finally {
+        } finally {
             em.close();
         }
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -8,6 +8,13 @@ import javax.persistence.Id;
 @Entity
 public class Member {
 
+    public Member(){
+    }
+    public Member(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
     @Id
     private Long id;
     private String name;
